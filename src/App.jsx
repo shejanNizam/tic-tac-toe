306c04dcf203e3200ae3d406171134d17e33f155
nizam <nizam.shejan11@gmail.com>
2025-06-18 08:50:30 +0600

    dynamic value set via props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-export default function App() {
+function Square({ value }) {
+  return (
+    <button className="bg-white border border-gray-500 w-12 h-12 leading-9 text-lg m-1">
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
   return (
     <>
-      <h1 className="text-2xl font-bold text-red-700">Hello tic-tac-toe</h1>
+      <div>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+
+      <div>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+
+      <div>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
     </>
   );
 }
